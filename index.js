18ca7be64aaef258f2365eb2a3f0c36eed745530
colmassakian <43759428+colmassakian@users.noreply.github.com>
2019-12-25 19:00:08 -0800

    Move static pages to public

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var app = require('express')();
-var http = require('http').Server(app);
+const express = require("express");
+const app = express();
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static("public"));
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
